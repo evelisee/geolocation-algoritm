bee7dd708e52cf6a23a071b667df5f47f92f300e
evelisee <evelisevazquez@gmail.comm>
2021-08-02 18:29:59 -0300

    feat: responsive footer

--- src/js/module/manipulationDom.js
+++ src/js/module/manipulationDom.js
@@ -1,17 +1,17 @@
-import { addPointerToMap } from '../module/map.js'
+import { addPointerToMap } from "../module/map.js";
 
-const buttonSearchGeolocation = document.getElementById('searchGeolocation');
-const coordinateInput = document.getElementById('coordinateInput');
-const mapAndListContainer = document.getElementById('mapAndListContainer');
+const buttonSearchGeolocation = document.getElementById("searchGeolocation");
+const coordinateInput = document.getElementById("coordinateInput");
+const mapAndListContainer = document.getElementById("mapAndListContainer");
 
-buttonSearchGeolocation.addEventListener('click',() => {
-    clearList();
-    const coordinate = [...coordinateInput.value.split(',')];
-    console.log(-23.5421282,-46.6971003);
-    addPointerToMap(coordinate);
-    mapAndListContainer.classList.remove('none');
+buttonSearchGeolocation.addEventListener("click", () => {
+  clearList();
+  const coordinate = [...coordinateInput.value.split(",")];
+  console.log(-23.5421282, -46.6971003);
+  addPointerToMap(coordinate);
+  mapAndListContainer.classList.remove("none");
 });
 
 function clearList() {
-    document.getElementById('storeInformationList').innerHTML = '';
+  document.getElementById("storeInformationList").innerHTML = "";
 }
--- src/mocks/store.js
+++ src/mocks/store.js
@@ -1,4 +1,3 @@
-
 export const mockStoresNike = [
     {
         name: 'Shopping Higienópilis',
@@ -22,29 +21,31 @@ export const mockStoresNike = [
         cep: '80010-180',
         phone: '(41) 3358-0621',
         availabilityInDays: 5,
-        coordinates: [-25.4314756, -49.2710761]
+        coordinates: [-25.4314756, -49.2710761],
     },
     {
         name: 'Guarulhos',
-        address: 'Rod. Pres. Dutra, KM 222,3 - Jardim Santa Francisca, Guarulhos - SP',
+        address:
+        'Rod. Pres. Dutra, KM 222,3 - Jardim Santa Francisca, Guarulhos - SP',
         cep: '07034-000',
         phone: '(11) 2189-0586',
         availabilityInDays: 7,
-        coordinates: [-23.4751418, -46.525286]
+        coordinates: [-23.4751418, -46.525286],
     },
     {
         name: 'IFashion Outlet Novo Hamburgo',
-        address: 'Rua Rincao, 505 Complemento: Salas 201 a 205 e 241 a 242 - Rincao - RS',
+        address:
+        'Rua Rincao, 505 Complemento: Salas 201 a 205 e 241 a 242 - Rincao - RS',
         cep: '93310-460',
         phone: '(51) 3303-3812',
         availabilityInDays: 2,
-        coordinates: [-29.6636174, -51.142409]
-    }
-]
+        coordinates: [-29.6636174, -51.142409],
+    },
+];
 export const stores = [
     [-23.5422422, -46.6598244],
     [-23.5628081, -46.6719082],
     [-25.4314756, -49.2710761],
     [-23.4751418, -46.525286],
-    [-29.6636174, -51.142409]
-]
+    [-29.6636174, -51.142409],
+];
